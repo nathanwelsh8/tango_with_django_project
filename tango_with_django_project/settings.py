02ff5becb39d3cfa216161a6f6724da80b12a068
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-01-18 13:13:01 +0000

    Added media and static directories

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR   = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR    = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,12 +66,18 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # added for media
             ],
         },
     },
 ]
 
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
+
+# Media
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL  = '/media/'
 
 
 # Database
@@ -119,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]

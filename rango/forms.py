a4a6d8370e0a7ca9c6571551444ba171f7840c20
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-01-26 14:20:47 +0000

    Chapter 8 complete and tested: OK

---
+++ rango/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name  = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+    
+    # HiddenInput means that the user cannot specify the value
+    # i.e is a default value. 
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug  = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # Inline class to provide additional info on the form
+    class Meta:
+        model = Category
+        fields = ('name',)
+    
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+    url   = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        #provide association between ModelForm and a model
+        model = Page
+
+        # fields = specifies fields to include
+
+        #specify that we do not want to include category field
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+from django.urls import reverse 
+
 
 
 # Create your views here.
@@ -18,8 +22,10 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage':'This tutorial has been put together by nathan.'}
-    return render(request, 'rango/about.html', context_dict)
+    #context_dict = {'boldmessage':'This tutorial has been put together by nathan.'}
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -48,3 +54,44 @@ def show_category(request, category_name_slug):
 
     # render response and return it to the client
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save(commit=True)
+
+            # possibly incorrect syntax here
+            return redirect('rango:index')#reverse('rango:index'))
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except:
+        category = None
+    
+    if category is None:
+        return redirect('rango:index')
+    
+    form = PageForm()
+    if request.method == 'POST':
+        form.PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))        
+        else:
+            print(form.errors)
+    context_dict = {'form':form, 'category':category}
+    return render(request, 'rango/add_page.html', context=context_dict)

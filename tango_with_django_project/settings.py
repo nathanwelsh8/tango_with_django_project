183d2bf5090f16f19ff46e5750014135d3ab13fe
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-01-28 15:22:09 +0000

    settings.py updated for password auth

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -71,6 +72,13 @@ TEMPLATES = [
         },
     },
 ]
+# Password stuff
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
@@ -100,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length':6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
